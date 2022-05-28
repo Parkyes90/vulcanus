37071f0389779f45e4d62c60bb7b6d575107136c
parkyes90 <parkyes90@gmail.com>
2022-05-28 13:11:33 +0900

    init proejct

--- services/posts/commands/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django_extensions.db.models import TimeStampedModel
-
-
-class Command(TimeStampedModel):
-    name = models.CharField(max_length=64)
-    command = models.CharField(max_length=512)
--- services/posts/commands/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from rest_framework import routers
-from .views import CommandViewSet
-
-router = routers.DefaultRouter()
-router.register("", CommandViewSet)
-
-urlpatterns = [path("", include(router.urls))]
